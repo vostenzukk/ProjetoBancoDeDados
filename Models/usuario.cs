8e739b4ededa66c60dfde2b04fe9e7bc9e469157
vostenzukk <marcosfelicianob@gmail.com>
2026-04-10 15:18:27 -0300

    projeto

---
+++ Models/usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SegundoappBandodeDados.Models
+{
+    public class Usuario
+    {
+        [Display(Name = "Código")]
+        public int? IdUsu { get; set; }
+
+        [Display(Name = "Nome Completo")]
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string nomeUsu { get; set; }
+
+        [Display(Name = "Cargo")]
+        [Required(ErrorMessage = "O campo Cargo é obrigatório")]
+        public string Cargo { get; set; }
+
+        [Display(Name = "Nascimento")]
+        [Required(ErrorMessage = "O campo nascimento é obrigatório")]
+        [DataType(DataType.DateTime)]
+        public DateTime DataNasc { get; set; }
+    }
+}
